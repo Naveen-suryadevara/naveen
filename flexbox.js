d9d67814c09edce8e8826f3346bc87e93e942591
Naveen-suryadevara <31907827+Naveen-suryadevara@users.noreply.github.com>
2017-10-02 19:36:47 -0500

    Add files via upload

---
+++ flexbox.js
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style> 
+.flex-container {
+    display: flex;
+    width: 1000px;
+    height: 800px;
+    background-color: lightgrey;
+    flex direction: coloumn;
+}
+
+.flex-item {
+    background-color: navy blue;
+    width: 200px;
+    height: 200px;
+    margin: 50px;
+}
+.item1 {
+        flex: 2;
+}
+
+.item2 {
+    flex: 1;
+}
+
+.item3 {
+    flex: 1;
+}
+
+</style>
+</head>
+<body>
+
+<div class="flex-container">
+  <div class="flex-item">
+<h1> The working of the box</h>
+<p> Pretty women wonder where my secret lies.I'm not cute or built to suit a fashion model
+</div>
+
+  <div class="flex-item">
+<h1> The working of the box</h>
+<p> Pretty women wonder where my secret lies.I'm not cute or built to suit a fashion model
+</div>
+
+  <div class="flex-item">
+<h1> The working of the box</h>
+<p> Pretty women wonder where my secret lies.I'm not cute or built to suit a fashion model
+</div>  
+
+</div>
+
+</body>
+</html>
